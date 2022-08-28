ed64cf9298eb7db773cb36812b804be24bc04b50
Helene <Helenecolberglaursen@hotmail.dk>
2022-08-28 16:14:40 +0200

    jeg har sat flere ting in og lavede ændringer så det ca er et skelet af hvad det skal være igen igen igen

--- includes/face.php
+++ includes/face.php
@@ -1,4 +1,7 @@
-<div class="row bg-dark">
-    <a class="navbar-brand col" href="#"><img src="images/newhairdontcarepup.png"> </a>
-    <div class="col small"><img src="images/kaosknap.png"> </div>
+
+<div class="container text-center">
+    <div class="row">
+        <div class="col-9"><a class="navbar-brand col" href="#"><img src="images/newhairdontcarepup.png"> </a></div>
+        <div class="col-3"><div class="col small"><img src="images/kaosknap.png"> </div></div>
+    </div>
 </div>
--- includes/header.php
+++ includes/header.php
@@ -3,23 +3,23 @@
 
 <nav class="navbar navbar-expand-lg bg-dark ">
     <div class="container-fluid ">
-        <a class="navbar-brand text-myYellow" href="#">Helene Colberg</a>
+        <a class="navbar-brand text-myYellow" href="#">Helene Colberg portfolio</a>
         <button class="navbar-toggler navbar-myYellow" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse " id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item ">
-                    <a class="nav-link active text-myYellow " aria-current="page" href="#">Home</a>
+                    <a class="nav-link active text-myYellow " aria-current="page" href="#">Arbejde</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-myYellow" href="#">Features</a>
+                    <a class="nav-link text-myYellow" href="#">CV</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-myYellow" href="#">Pricing</a>
+                    <a class="nav-link text-myYellow" href="#">Om mig</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled text-myYellow">Disabled</a>
+                    <a class="nav-link text-myYellow" href="#">Kontakt</a>
                 </li>
             </ul>
         </div>
