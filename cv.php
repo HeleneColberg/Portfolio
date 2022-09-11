e0efbc4c06958cd804707d880bb27e84014b7e28
Helene <Helenecolberglaursen@hotmail.dk>
2022-09-11 22:53:45 +0200

    kompetancere i CV og publikationer

--- cv.php
+++ cv.php
@@ -16,11 +16,57 @@
     <div class="row row-cols-1 row-cols-sm-2">
         <!-- Gilleleje isen-->
         <div class="col bg-yp1 p-5 mb-5 text-pup">
-            <a href="gillelejeisen.php">
-                <h1 class="p-4 link-pup">Gilleleje isen</h1>
+            <h1 class=" p-4">It kompetancer</h1>
+            <div class="row">
+                <div class="col">
+                    <li>Adobe Illustrator</li>
+                    <li>Adobe Photoshop</li>
+                    <li>Adobe Lightroom</li>
+                    <li>Adobe XD</li>
+                    <li>Adobe Indesign</li>
+                    <li>HTML Bootstrap</li>
+                    <li>Python</li>
+                    <li>Matlab</li>
 
-                <img src="images/glios.png">
-            </a>
+
+                </div>
+                <div class="col">
+                    <li>Office pakken</li>
+                    <li>Latex</li>
+                    <li>AutoCAD</li>
+                    <li>Revit</li>
+                    <li>Inventor</li>
+
+
+
+                </div>
+                <div class="w-100"></div>
+            </div>
+            <h1 class=" p-4">Personlige kompetancer</h1>
+            <div class="row">
+                <div class="col">
+                    <li>Kreativ</li>
+                    <li>Energisk</li>
+                    <li>Imødekommende</li>
+                    <li>Social</li>
+                    <li>Tålmodig</li>
+                    <li>Åben/udadvendt</li>
+
+
+                </div>
+                <div class="col">
+                    <li>Selvstændig</li>
+                    <li>Hårdtarbejdende</li>
+                    <li>Ansvarsbevidst</li>
+                    <li>Mødestabil</li>
+                    <li>Lærenem</li>
+                    <li>Omstillingsparat</li>
+
+
+                </div>
+                <div class="w-100"></div>
+            </div>
+
         </div>
         <!-- Chokolate Club-->
         <div class="col bg-yp2 p-5 mb-5 text-myYellow">
@@ -40,7 +86,7 @@
             </a>
         </div>
 
-        <!-- logo design og personlige projekter-->
+        <!-- sprog-->
         <div class="col bg-yp4 p-5 mb-5 text-myYellow">
             <a href="arbejde.php">
                 <h1 class="p-4 link-myYellow">Personlige projekter og logoer</h1>
@@ -49,15 +95,30 @@
             </a>
         </div>
 
-        <!-- Jobs -->
+        <!-- puplicationer -->
         <div class="col bg-yp5 p-5 mb-5 text-myYellow">
-            <a href="arbejde.php">
-                <h1 class="p-4 link-myYellow">Jobs</h1>
+            <h1 class=" p-4">Publikationer</h1>
+            <div class="row">
+                <div class="col link-myYellow">
+                    <li > <a class="link-myYellow" href="https://doi.org/10.3390/nano8020109">Kühnel, R. M., M. Ahmad, F. Andersen, Á. B. Bech, L. H. K. Bendixen, R. P.
+                            Nawrocki, J. A. Bloch, I. Bora, A. T. Bukhari, V. N. Bærentsen, J. Carstensen,
+                            S. Chima, H. Colberg, T. R. Dahm, A. J. Daniels, N. Dinckan, M. El Idrissi, R.
+                            Erlandsen, M. Førster, Y. Ghauri, M. Gold, A. Hansen, K. Hansen, M. Helmsøe-
+                            Zinck, M. Henriksen, V. S. Hoffmann, O. L. Hyllested, C. Jensen, S. A. Kallenbach,
+                            K. Kaur, R. S. Khan, T. E. Kjær, B. Kristiansen, S. Langvad, M. P. Lund, F.
+                            C. Munk, T. Møller, M. O. Nehme, R. M. Nejrup, L. Nexø, S. S. H. Nielsen,
+                            N. Niemeier, V. L. Nikolajsen, C. P. Nøhr, B. D. Orlowski, M. Overgaard, J. S.
+                            Ovesen, L. Paustian, S. A. Pedersen, K. M. Petersen, M. C. Poulsen, L. Praeger-
+                            Jahnsen, S. L. Qureshi, N. Ree, S. L. Schiermacher, B. M. Simris, G. Smith, N. H.
+                            Smith, K. A. Sonne, R. M. Zenulovic, W. A. Sørensen, K. Sørensen, E. Vogt, A.
+                            Væring, J. Westermann, B. S. Özcan and T. J. Sørensen (2018). "Using Polarized
+                            Spectroscopy to Investigate Order in Thin-Films of Ionic Self-Assembled Materials
+                            Based on Azo-Dyes." Nanomaterials 8(2). </a></li>
 
-                <img src="images/jobs.png">
-            </a>
+
+                </div>
         </div>
-
+        </div>
         <?php include "includes/l6kausel.php" ?>
     </div>
 </div>
